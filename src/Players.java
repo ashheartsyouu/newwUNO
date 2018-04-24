0817cfa1d017b603958b4698bbeb7dd8124de8cd
egc04856 <egc04856@uga.edu>
2018-04-24 19:57:22 -0400

    Update Players.java

--- src/Players.java
+++ src/Players.java
@@ -55,6 +55,7 @@ public class Players {
 	
 	public void printHand()
 	{
+		System.out.println("-------------------------------------------");
 		System.out.println(name + "'s Hand:");
 		int i = 1;
 		for(Cards aCard: hand)
@@ -63,6 +64,7 @@ public class Players {
 			aCard.printInfo();
 			++i;
 		}
+		System.out.println("-------------------------------------------");
 	}
 
 	public void generateHand() {
